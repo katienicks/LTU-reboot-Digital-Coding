398cf215cdab9968c2c1bca68d4846733e6adc86
katienicks <72015405+katienicks@users.noreply.github.com>
2020-10-12 11:34:44 +0100

    first js file

---
+++ module-3/scripts/main.js
@@ -0,0 +1,15 @@
+
+var numberOfSmarties = 40;
+var numberOfIceCreams = 30;
+var total = numberOfSmarties + numberOfIceCreams;
+
+console.log(total);
+
+/*
++ add
+= subtract
+/ divide
+* mutliply
+% modulus (amount left over after dividing)
+
+*/
