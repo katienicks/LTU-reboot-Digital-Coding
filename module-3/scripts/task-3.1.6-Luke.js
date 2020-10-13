d54c62dd4f0532873a8863f5de30fe79ae24e2b4
katienicks <72015405+katienicks@users.noreply.github.com>
2020-10-13 16:51:44 +0100

    Created Task 3.1.6 Answer

---
+++ module-3/scripts/task-3.1.6-Luke.js
@@ -0,0 +1,10 @@
+var billBeforeTip = 40; //Bill before tip//
+
+var tip = 12.5; //% of tip//
+
+var tipAmount = (tip * billBeforeTip) / 100
+
+var receipt = 'The bill plus tip is ' + (billBeforeTip + tipAmount);
+
+console.log( receipt );
+
