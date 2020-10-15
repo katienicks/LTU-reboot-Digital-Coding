0cf598eb9b74ec773dfdc1ee8af4b0665f34fd87
katienicks <72015405+katienicks@users.noreply.github.com>
2020-10-15 17:45:04 +0100

    created index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+var numberOfNuggets = 12;
+var numberOfEaters = 2;
+var total = numberOfNuggets / numberOfEaters;
+
+
+var result = 'There are ' + numberOfNuggets + ' and there are' + numberOfEaters ' of us eating. That means we can have ' + total + ' nuggets each.';
+
+
+document.write( result );
+
