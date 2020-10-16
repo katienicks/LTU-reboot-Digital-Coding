4e89bc3f41c3a9b1bf08a070b5d15ce2c68c07be
katienicks <72015405+katienicks@users.noreply.github.com>
2020-10-16 09:50:33 +0100

    fixed js calculation for nuggets

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+
+
 var numberOfNuggets = 12;
 var numberOfEaters = 2;
 var total = numberOfNuggets / numberOfEaters;
 
 
-var result = 'There are ' + numberOfNuggets + ' and there are' + numberOfEaters ' of us eating. That means we can have ' + total + ' nuggets each.';
+var result = 'There are ' + numberOfNuggets + ' and there are ' + numberOfEaters + ' of us eating. That means we can have ' + total + ' nuggets each.';
 
 
 document.write( result );
+
 
